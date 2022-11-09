11cb7d9d7dfcce3d852864902c52fb6e1afaba52
Yaroslav <104159378+Y-Havryliv@users.noreply.github.com>
2022-11-09 14:10:39 +0200

    Update models.py

--- db/models.py
+++ db/models.py
@@ -31,6 +31,7 @@ class CinemaHall(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
+    @property
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
